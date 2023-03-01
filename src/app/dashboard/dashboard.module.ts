ac23a2924e0cb91391c413447088253207ac2b4e
Mohamed Driwach <mdriwach@endepro.com>
2023-03-01 22:39:49 +0100

    Changes

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Route, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+
+
+
+const routes: Route[] = [
+    {
+      path:'',
+      loadComponent: async ()=> DashboardComponent,
+    }
+]
+
+@NgModule({
+  declarations: [DashboardComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class DashboardModule { }
